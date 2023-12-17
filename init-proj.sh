35d01c45fe93ae038dcfb8e1e823d050790723bc
Moon-edu <102866148+Moon-edu@users.noreply.github.com>
2023-12-17 22:29:01 +0700

    Create init-proj.sh

---
+++ init-proj.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Update package lists
+sudo apt update -y
+
+# Upgrade installed packages
+sudo apt upgrade -y
+
+# Install Python 3
+sudo apt install -y python3
+
+# Create a symbolic link for 'python' command to point to 'python3'
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 1
+
+# Install git-lfs
+sudo apt install -y git-lfs
+
+# Install libpq-dev
+sudo apt install -y libpq-dev
+
+# Install OpenJDK 8
+sudo apt install -y openjdk-8-jdk
+
+# Install zip
+sudo apt install -y zip
+
+# Install xz
+sudo apt install -y xz-utils
